2fe5969770b70e7a0628743b903c53c27dbb30f5
AustinKunkel <austinkunkel85@gmail.com>
2025-03-20 20:48:56 -0400

    added js fetch requests for post and delete

--- c/main_project/led_control/www/script.js
+++ c/main_project/led_control/www/script.js
@@ -1,3 +1,15 @@
 function showMessage() {
   alert("Hello from MicroHTTPD!");
 }
+
+fetch("/data.json", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ message: "Hello, server!" })
+})
+.then(response => response.text())
+.then(data => console.log(data));
+
+fetch("/data.json", { method: "DELETE" })
+.then(response => response.text())
+.then(data => console.log(data));
