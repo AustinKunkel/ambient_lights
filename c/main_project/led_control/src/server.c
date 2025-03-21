3517bced25d21006a7efdda2028f1b544d874bf7
YoItsUncle <austinkunkel85@gmail.com>
2025-03-21 14:19:22 -0400

    changed request_handler signature

--- c/main_project/led_control/src/server.c
+++ c/main_project/led_control/src/server.c
@@ -29,10 +29,10 @@ const char *get_content_type(const char *path) {
 }
 
 // Function to serve files from the "www" directory
-static int request_handler(void *cls, struct MHD_Connection *connection,
-                           const char *url, const char *method,
-                           const char *version, const char *upload_data,
-                           size_t *upload_data_size, void **con_cls) {
+int request_handler(void *cls, struct MHD_Connection *connection,
+    const char *url, const char *method,
+    const char *version, const char *upload_data,
+    size_t *upload_data_size, void **con_cls) {
 
   if (strcmp(method, "GET") == 0) {
   return handle_get_request(connection, url);
