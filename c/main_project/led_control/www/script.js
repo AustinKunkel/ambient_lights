7d02b7b883e04fd5420e0637a3f95c56e2e2e487
AustinKunkel <austinkunkel85@gmail.com>
2025-03-20 20:55:59 -0400

    added button for post and delete request

--- c/main_project/led_control/www/script.js
+++ c/main_project/led_control/www/script.js
@@ -2,14 +2,20 @@ function showMessage() {
   alert("Hello from MicroHTTPD!");
 }
 
-fetch("/data.json", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ message: "Hello, server!" })
-})
-.then(response => response.text())
-.then(data => console.log(data));
+function sendPost() {
+  fetch("/data.json", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "Hello, server!" })
+  })
+  .then(response => response.text())
+  .then(data => console.log(data));
+}
 
-fetch("/data.json", { method: "DELETE" })
-.then(response => response.text())
-.then(data => console.log(data));
+
+function sendDelete() {
+  fetch("/data.json", { method: "DELETE" })
+  .then(response => response.text())
+  .then(data => console.log(data));
+  
+}
