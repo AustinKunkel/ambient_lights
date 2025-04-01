3d8059a8e91e8b578d0d45589fb6f4c997f029db
AustinKunkel <austinkunkel85@gmail.com>
2025-03-31 20:03:19 -0400

    extern in h file

--- c/main_project/led_control/test/led_test.h
+++ c/main_project/led_control/test/led_test.h
@@ -2,7 +2,7 @@
 #ifndef LED_TEST_H
 #define LED_TEST_H
 
-ws2811_t ledstring;
+extern ws2811_t ledstring;
 void cleanup_strip();
 int setup_strip();
 void set_led_color(int, uint8_t, uint8_t, uint8_t);
