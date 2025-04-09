354948f9db1e3249a6f8c658604f905252752dc9
YoItsUncle <austinkunkel85@gmail.com>
2025-04-09 14:07:53 -0400

    testing check index

--- c/main_project/led_control/test/led_capture_test.c
+++ c/main_project/led_control/test/led_capture_test.c
@@ -258,10 +258,11 @@ uint32_t blend_colors(struct led_position* led_list, unsigned char *rgb_buffer, 
   int count = 0;
   for(int i = -depth; i < depth + 1; i++) {
     int check_index = (index + i) % LED_COUNT;
+    printf("check index: %d\t", check_index);
     struct led_position check_pixel_location = led_list[check_index];
 
     int buffer_index = (check_pixel_location.y * WIDTH + check_pixel_location.x) * 3;
-    printf("check index:%d, buffer index: %d, count: %d\t", check_index, buffer_index, count);
+    // printf("check index:%d, buffer index: %d, count: %d\t", check_index, buffer_index, count);
     r_total += rgb_buffer[buffer_index];
     g_total += rgb_buffer[buffer_index + 1];
     b_total += rgb_buffer[buffer_index + 2];
