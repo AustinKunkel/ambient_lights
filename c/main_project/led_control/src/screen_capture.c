0fd46b9adb5f22fabc488f6fe6b1210236c47207
AustinKunkel <austinkunkel85@gmail.com>
2025-04-15 01:10:41 -0400

    added some leniance to colors

--- c/main_project/led_control/src/screen_capture.c
+++ c/main_project/led_control/src/screen_capture.c
@@ -120,9 +120,12 @@ int auto_align_offsets() {
     for(int j = 0; j < HEIGHT; j++) { // y
       //index = (y * WIDTH + x) * 3;
       int index = (j * WIDTH + i) * 3;
-      uint32_t color = rgb_buffer[index] << 16 | rgb_buffer[index + 1] << 8 | rgb_buffer[index + 2];
-      if(color > 0) {
-        not_black = true;
+      uint8_t r = rgb_buffer[index];
+      uint8_t g = rgb_buffer[index + 1];
+      uint8_t b = rgb_buffer[index + 2];
+
+      if (r > 10 || g > 10 || b > 10) {
+        row_is_not_black = true;
         break;
       }
     }
