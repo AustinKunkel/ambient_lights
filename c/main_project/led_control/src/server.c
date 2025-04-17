6885bcf2d7fc9e5b5683a2d817203367009098a9
YoItsUncle <austinkunkel85@gmail.com>
2025-04-17 18:55:06 -0400

    lets try this

--- c/main_project/led_control/src/server.c
+++ c/main_project/led_control/src/server.c
@@ -10,31 +10,41 @@
 #include "main.h"
 #include "csv_control.h"
 
-static int callback_http(struct lws *wsi,
-                         enum lws_callback_reasons reason,
-                         void *user, void *in, size_t len) {
-    return lws_callback_http_dummy(wsi, reason, user, in, len);
-} 
-
-static int callback_ws(struct lws *wsi,
-                       enum lws_callback_reasons reason,
-                       void *user, void *in, size_t len) {
-    switch (reason) {
-        case LWS_CALLBACK_ESTABLISHED:
-            lwsl_user("Websocket Connected\n");
-            break;
-        case LWS_CALLBACK_RECEIVE:
-            lws_write(wsi, (unsigned char *)in, len, LWS_WRITE_TEXT);
-            break;
-        default:
-            break;
+static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
+    void *user, void *in, size_t len) {
+switch (reason) {
+    case LWS_CALLBACK_HTTP:
+        if (lws_serve_http_file(wsi, "index.html", "text/html"))
+            return -1;
+        break;
+    default:
+        break;
     }
     return 0;
 }
 
+static int callback_ws(struct lws *wsi, enum lws_callback_reasons reason,
+ void *user, void *in, size_t len) {
+switch (reason) {
+    case LWS_CALLBACK_ESTABLISHED:
+        // Handle new WebSocket connection
+        break;
+    case LWS_CALLBACK_RECEIVE:
+        // Handle received WebSocket message
+        break;
+    case LWS_CALLBACK_CLOSED:
+        // Handle WebSocket closure
+        break;
+    default:
+        break;
+}
+return 0;
+}
+
+
 static struct lws_protocols protocols[] = {
     {
-        .name = "http",
+        .name = "http-only",
         .callback = lws_callback_http_dummy,  // built-in HTTP handler
         .per_session_data_size = 0,
         .rx_buffer_size = 0,
@@ -45,36 +55,27 @@ static struct lws_protocols protocols[] = {
         .per_session_data_size = 0,          // adjust if needed
         .rx_buffer_size = 4096,
     },
-    LWS_PROTOCOL_LIST_TERM
+    { NULL, NULL, 0, 0 }
 };
 
 int main() {
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof(info));
-    
 
     info.port = 8080;
     info.protocols = protocols;
-    info.mounts = &(struct lws_http_mount) {
-        .mount_next = NULL,
-        .mountpoint = "/",
-        .origin = "/home/controller/ambient_lights/c/main_project/led_control/www",  // your static files folder
-        .def = "index.html",
-        .origin_protocol = LWSMPRO_FILE,
-        .protocol = NULL,
-        .cgienv = NULL,
-        .extra_mimetypes = NULL,
-        .interpret = NULL,
-        .cgi_timeout = 0,
-        .cache_max_age = 0,
-        .auth_mask = 0,
-        .cache_reusable = 0,
-        .cache_revalidate = 0,
-        .cache_intermediaries = 0,
-        .basic_auth_login_file = NULL
-    };
-
-    lws_set_log_level(LLL_ERR | LLL_WARN | LLL_NOTICE | LLL_DEBUG, NULL);
+    struct lws_context *context = lws_create_context(&info);
+    if (context == NULL) {
+        fprintf(stderr, "libwebsockets init failed\n");
+        return -1;
+    }
+    
+    while (1) {
+        lws_service(context, 0);
+    }
+    
+    lws_context_destroy(context);
+    
 
     struct lws_context *context = lws_create_context(&info);
     if (!context) {
