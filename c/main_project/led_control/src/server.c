1753929a892a7b23199dc6b69249645bffff7fe3
YoItsUncle <austinkunkel85@gmail.com>
2025-04-17 19:08:01 -0400

    trying to fix 404

--- c/main_project/led_control/src/server.c
+++ c/main_project/led_control/src/server.c
@@ -12,30 +12,22 @@
 
 static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
     void *user, void *in, size_t len) {
-    switch (reason) {
-        case LWS_CALLBACK_HTTP: {
-            const char *requested_uri = (const char *)in;
-            char filepath[512];
-        
-            // Construct the full file path
-            snprintf(filepath, sizeof(filepath), "/home/controller/ambient_lights/c/main_project/led_control/www%s", requested_uri);
-        
-            // Determine the content type based on the file extension
-            const char *content_type = "text/plain";
-            if (strstr(requested_uri, ".html"))
-                content_type = "text/html";
-            else if (strstr(requested_uri, ".css"))
-                content_type = "text/css";
-            else if (strstr(requested_uri, ".js"))
-                content_type = "application/javascript";
-        
-            // Serve the file
-            if (lws_serve_http_file(wsi, filepath, content_type, NULL, 0) < 0)
-                return -1; // Error occurred
-        
-            return 1; // Close the connection after serving the file
+        switch (reason) {
+
+            case LWS_CALLBACK_HTTP:
+                // LWS handles static file serving via the mount
+                lwsl_info("HTTP request for: %s\n", (const char *)in);
+                break;
+    
+            case LWS_CALLBACK_HTTP_FILE_COMPLETION:
+                // Optional: called after serving a file
+                return -1;  // close connection after serving
+    
+            default:
+                break;
         }
-    }
+    
+        return 0;
 }
 
 static int callback_ws(struct lws *wsi, enum lws_callback_reasons reason,
@@ -64,12 +56,12 @@ static struct lws_protocols protocols[] = {
         .per_session_data_size = 0,
         .rx_buffer_size = 0,
     },
-    {
-        .name = "my-websocket-protocol",
-        .callback = callback_ws,
-        .per_session_data_size = 0,          // adjust if needed
-        .rx_buffer_size = 4096,
-    },
+    // {
+    //     .name = "my-websocket-protocol",
+    //     .callback = callback_ws,
+    //     .per_session_data_size = 0,          // adjust if needed
+    //     .rx_buffer_size = 4096,
+    // },
     { NULL, NULL, 0, 0 }
 };
 
@@ -77,8 +69,31 @@ int main() {
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof(info));
 
+    static const struct lws_http_mount mount = {
+        .mount_next = NULL,        // linked-list of mounts
+        .mountpoint = "/",         // URL mount point
+        .origin = "/home/controller/ambient_lights/c/main_project/led_control/www", // local path
+        .def = "index.html",       // default file
+        .protocol = NULL,
+        .cgienv = NULL,
+        .extra_mimetypes = NULL,
+        .interpret = NULL,
+        .cgi_timeout = 0,
+        .cache_max_age = 0,
+        .auth_mask = 0,
+        .cache_reusable = 0,
+        .cache_revalidate = 0,
+        .cache_intermediaries = 0,
+        .origin_protocol = LWSMPRO_FILE, // Serve from filesystem
+        .basic_auth_login_file = NULL,
+    };
+
     info.port = 8080;
+    info.mounts = &mount;
     info.protocols = protocols;
+    info.gid = -1;
+    info.uid = -1;
+    info.options = LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
     struct lws_context *context = lws_create_context(&info);
     if (context == NULL) {
         fprintf(stderr, "libwebsockets init failed\n");
@@ -86,7 +101,7 @@ int main() {
     }
     
     while (1) {
-        lws_service(context, 0);
+        lws_service(context, 100);
     }
     
     lws_context_destroy(context);
