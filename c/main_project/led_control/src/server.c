3160f382f60ed5a39f1eb313ca8a9949201dec65
YoItsUncle <austinkunkel85@gmail.com>
2025-04-17 21:33:48 -0400

    adding more functionality to web socket server

--- c/main_project/led_control/src/server.c
+++ c/main_project/led_control/src/server.c
@@ -3,11 +3,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+#include "../../cJSON/cJSON.h"
+#include "led_functions.h"
+#include "main.h"
+#include "csv_control.h"
 
 static const struct lws_protocols protocols[];
 
 #define STATIC_PATH "./led_control/www"  // Path for static files
-#define WEBSOCKET_PORT 8080
+#define PORT 8080
+
+struct lws_context *context;
+
+int initialize_led_settings() {
+    char data_line[512];
+    printf("reading led_settings.csv...\n");
+    if(read_one_line(LED_SETTINGS_FILENAME, data_line, sizeof(data_line)) == 0)
+    {
+        char *line_ptr = data_line;
+        printf("Setting led settings variables...\n");
+        led_settings.brightness = atoi(next_token(&line_ptr));
+        printf("Brightness: %d\n", led_settings.brightness);
+        char *hex = next_token(&line_ptr);
+        //printf("Hex string: %s\n", hex);
+        if(hex) {
+            char *hex_ptr = (hex[0] == '#') ? hex + 1 : hex;
+            //printf("Hex ptr: %s\n", hex_ptr);
+            led_settings.color = (int)strtol(hex_ptr, NULL, 16);
+        } else {
+            led_settings.color = 0xDFC57B;
+        }
+        //printf("Color: #%06lX\n", led_settings.color);
+        led_settings.capture_screen = atoi(next_token(&line_ptr));
+        //printf("Capture screen: %d\n", led_settings.capture_screen);
+        led_settings.sound_react = atoi(next_token(&line_ptr));
+        //printf("sound react: %d\n", led_settings.sound_react);
+        led_settings.fx_num = atoi(next_token(&line_ptr));
+        //printf("fx num: %d\n", led_settings.fx_num);
+        led_settings.count = atoi(next_token(&line_ptr));
+        //printf("count: %d\n", led_settings.count);
+        led_settings.id = atoi(next_token(&line_ptr));
+        //printf("id: %d\n", led_settings.id);
+        char buffer[256];
+        parse_led_settings_data_to_string(buffer);
+        printf("Current LED settings: %s\n", buffer);
+        return 0;
+    } else {
+        perror("Unable to read from led_settings.csv!!\n");
+        return 1;
+    }
+}
 
 // WebSocket protocol callback function
 static int websocket_callback(struct lws *wsi, enum lws_callback_reasons reason,
@@ -29,6 +76,12 @@ static int websocket_callback(struct lws *wsi, enum lws_callback_reasons reason,
     }
 
     return 0;
+}
+
+void stop_server(int signo) {
+    printf("\nStopping server...\n");
+    lws_context_destroy(context);
+    exit(0);
 }
 
 // HTTP callback function for serving static files
@@ -122,7 +175,7 @@ static struct lws_context *create_server_context()
 {
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof(info));
-    info.port = WEBSOCKET_PORT;
+    info.port = PORT;
     info.protocols = protocols;
     info.gid = -1;
     info.uid = -1;
@@ -140,10 +193,29 @@ static struct lws_context *create_server_context()
 
 int main(void)
 {
-    struct lws_context *context = create_server_context();
+    signal(SIGINT, stop_server);
+    signal(SIGTERM, stop_server);
+    context = create_server_context();
     if (!context) {
         return -1;
     }
+
+    if(initialize_led_settings()) {
+        printf("Error initializing Led settings!\n");
+        return 1;
+    }
+
+    printf("Server running on localhost:%d/\n", PORT);
+    printf("Press CTRL+C to stop the server.\n");
+
+    if(setup_strip(led_settings.count) != 0) {
+        printf("Problem setting up strip...\n");
+        return 1;
+    }
+
+    set_strip_32int_color(led_settings.color);
+    set_brightness(led_settings.brightness);
+    show_strip();
 
     // Main event loop to process connections
     while (1) {
