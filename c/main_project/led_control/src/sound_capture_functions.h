c98d7ed3ced75a7cf58c467dab6cd59637df3bcb
YoItsUncle <austinkunkel85@gmail.com>
2025-09-02 20:49:23 -0400

    trying to fixe multiple definitions issue

--- c/main_project/led_control/src/sound_capture_functions.h
+++ c/main_project/led_control/src/sound_capture_functions.h
@@ -1,5 +1,5 @@
 #pragma once
 
 void setup_audio_capture(unsigned int sample_rate, unsigned int channels);
-void capture_frame(float *buffer, int frame_size);
+void capture_audio_frame(float *buffer, int frame_size);
 void cleanup_audio();
