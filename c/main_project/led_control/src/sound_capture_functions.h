e65eddcfee21d2a982a84382cc3c735a53abe4e5
AustinKunkel <austinkunkel85@gmail.com>
2025-09-03 12:43:38 -0400

    hopefully this works

--- c/main_project/led_control/src/sound_capture_functions.h
+++ c/main_project/led_control/src/sound_capture_functions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 void setup_audio_capture(unsigned int sample_rate, unsigned int channels);
 void capture_audio_frame(int16_t *buffer, int frame_size);
 void cleanup_audio();
